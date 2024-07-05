36d91aabd43e43e9d8fbca20a234dd9b3d8891ea
Lolopel <lolo_pel_1811@hotmail.com>
2024-07-05 22:15:47 +0200

    Master commit

---
+++ src/test/java/com/SpringFramework5/Spring_PetClinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringFramework5.Spring_PetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
